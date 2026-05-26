bc0bf83588579fea7c12c2315d6a2770e6213646
joravv <joravulaa@gmail.com>
2026-05-26 20:23:15 +0200

    mod4,5

--- functions.php
+++ functions.php
@@ -1,11 +1,27 @@
 <?php
-function add_theme_scripts(){
-wp_enqueu_style("style",get_stylesheet_url());
+// function add_theme_scripts(){
+// wp_enqueue_style("style",get_stylesheet_url());
 
 
-wp_enqueu_style("style",get_stylesheet_url());
+// wp_enqueue_style("style",get_stylesheet_url());
 
+// wp_enqueue_style("style",get_stylesheet_url());
+// }
+
+add_theme_support('post-thumbnails');
+
+function thembes_enqueue_styles(){
+    wp_enqueue_style("bootstrap", get_template_directory_uri(). './css/bootstrap.min.css');
+        wp_enqueue_style("core", get_template_directory_uri(). './style.css');
 }
-wp_enqueu_style("style",get_stylesheet_url());
+add_action('wp_enqueue_scripts', 'thembes_enqueue_styles');
+function theme_scripts(){
+    wp_enqueue_script("bootstrap", get_template_uri(). './js/bootstrap.bundle.min.js');
+array(("jquery"));
+
+add_action('wp_enqueue_scripts', 'theme_scripts');
+}
+
+
 
 ?>
